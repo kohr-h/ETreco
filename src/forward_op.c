d1d29fffad5e60eebfd7f10d0b30ab846bf69660
Holger Kohr <kohr@num.uni-sb.de>
2014-01-24 17:38:57 +0100

    WIP: implement forward operator

--- src/forward_op.c
+++ src/forward_op.c
@@ -31,7 +31,68 @@ int main(int argc, char **argv)
 {
   CEXCEPTION_T _e = EXC_NONE;
 
+  int last_index;
+
+  FwdOpts *opts = NULL;
+  EtParams *et_params = NULL;
+  FwdParams *fwd_params = NULL;
+  tiltangles *tilts = NULL;
+  gfunc3 *volume = NULL, *tiltseries = NULL;
   
+  Try { 
+    opts = new_FwdOpts (); 
+    et_params = new_EtParams (); 
+    fwd_params = new_FwdParams (); 
+    tilts = new_tiltangles ();
+    volume = new_gfunc3 ();
+    tiltseries = new_gfunc3 ();
+  } CATCH_EXIT_FAIL (_e);
+
+
+  /* Assign options, parameters, tiltangles and init gfunc structures */
+  Try { FwdOpts_assign_from_args (opts, argc, argv); } CATCH_EXIT_FAIL (_e);
+  Try { 
+    EtParams_assign_from_file (et_params, opts->fname_params); 
+    FwdParams_assign_from_file (fwd_params, opts->fname_params); 
+    tiltangles_assign_from_file (tilts, opts->fname_tiltangles);
+    gfunc3_init_mrc (volume, opts->fname_in, NULL, NULL, VOLUME);
+    // gfunc3_init (tiltseries, NULL, opts)
+  }  CATCH_EXIT_FAIL (_e);
+  
+  
+  /* Scale and shift the volume if necessary */
+  Try {
+    FwdParams_apply_to_volume (fwd_params, volume);
+    gfunc3_scale_grid (volume, et_params->magnification); 
+  }  CATCH_EXIT_FAIL (_e);
+
+
+  /* Set some parameters and resolve conflicts */
+  if (opts->num_images == 0)  /* Not specified by option, thus taken from data */
+    opts->num_images = tilts->ntilts - opts->start_index;
+
+  last_index = opts->start_index + opts->num_images - 1;
+
+  Try {
+    if (last_index >= tilts->ntilts)
+      EXC_THROW_CUSTOMIZED_PRINT (EXC_BADARG, "Image indices (max: %d) must be "
+      "smaller\n than the number of tiltangles (%d).\n", last_index, tilts->ntilts);
+  } CATCH_EXIT_FAIL (_e);
+
+
+  /* Print a summary of everything before starting */
+  FwdOpts_print (opts);
+  EtParams_print (et_params);
+  FwdParams_print (fwd_params);
+  gfunc3_print_grid (volume, "volume grid:");
+  
+  FwdOpts_free (&opts);
+  EtParams_free (&et_params);
+  FwdParams_free (&fwd_params);
+  tiltangles_free (&tilts);
+  gfunc3_free (&volume);
+  
+  return EXIT_SUCCESS;
   
   /*
   gfunc3 *phantom = new_gfunc3 (), *proj_img = new_gfunc3 (), *proj_r = new_gfunc3();
