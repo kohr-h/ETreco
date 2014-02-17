8a85576083a3cf9aaedf2758609d9ffacbac89d3
Holger Kohr <kohr@num.uni-sb.de>
2014-02-17 11:02:48 +0100

    WIP: implement Landweber method

--- src/landweber_single_axis.c
+++ src/landweber_single_axis.c
@@ -37,14 +37,14 @@ int main(int argc, char **argv)
   int i, k;
   float theta_last = 0.0, theta_cur = 0.0, theta_next = 0.0, *weights = NULL;
 
-  idx3 res_shp;
   vec3 angles;
 
   LandwOpts *opts = NULL;
   EtParams *et_params = NULL;
   LandwParams *landw_params = NULL;
   tiltangles *tilts = NULL;
-  gfunc3 *volume = NULL, *volume_r = NULL, *proj_stack = NULL, *cur_img = NULL, *residual = NULL;
+  gfunc3 *volume = NULL, *proj_stack = NULL, *residual_stack = NULL;
+  gfunc3 *cur_img1 = NULL, *cur_img2 = NULL;
   
   Try { 
     opts = new_LandwOpts (); 
@@ -52,10 +52,10 @@ int main(int argc, char **argv)
     landw_params = new_LandwParams (); 
     tilts = new_tiltangles ();
     volume = new_gfunc3 ();
-    volume_r = new_gfunc3 ();
     proj_stack = new_gfunc3 ();
-    cur_img = new_gfunc3 ();
-    residual = new_gfunc3 ();
+    cur_img1 = new_gfunc3 ();
+    cur_img2 = new_gfunc3 ();
+    residual_stack = new_gfunc3 ();
   } CATCH_EXIT_FAIL (_e);
 
 
@@ -66,7 +66,7 @@ int main(int argc, char **argv)
     LandwParams_assign_from_file (landw_params, opts->fname_params); 
     tiltangles_assign_from_file (tilts, opts->fname_tiltangles);
     gfunc3_init_mrc (proj_stack, opts->fname_in, NULL, NULL);
-  }  CATCH_EXIT_FAIL (_e);
+  } CATCH_EXIT_FAIL (_e);
 
 
   /* Check consistency in number of tilts */
@@ -88,14 +88,6 @@ int main(int argc, char **argv)
   Try {
     LandwParams_apply_to_volume (landw_params, volume);
     gfunc3_scale_grid (volume, et_params->magnification);
-  }  CATCH_EXIT_FAIL (_e);
-
-
-  /* Initialize the residual (only one 2D image) */
-  idx3_copy (res_shp, proj_stack->shape);
-  res_shp[2] = 1;
-  Try { 
-    gfunc3_init (residual, proj_stack->x0, proj_stack->csize, res_shp, REAL); 
   } CATCH_EXIT_FAIL (_e);
 
 
@@ -107,19 +99,24 @@ int main(int argc, char **argv)
   gfunc3_print_grid (proj_stack, "Projection stack grid:");
 
 
-  /* Normalize the stack and make it complex */
+  /* Normalize the stack */
   if (normalize_flag)
     {
       printf ("\n\nNormalizing...\n\n");
       for (i = 0; i < tilts->ntilts; i++)
         {
-          Try { gfunc3_set_stack_pointer (cur_img, proj_stack, i); }  CATCH_EXIT_FAIL (_e);
+          Try { gfunc3_set_stack_pointer (cur_img1, proj_stack, i); }  CATCH_EXIT_FAIL (_e);
           Try { 
-            histogram_normalization (cur_img, opts->bg_patch_ix0, opts->bg_patch_shape);
+            histogram_normalization (cur_img1, opts->bg_patch_ix0, opts->bg_patch_shape);
           } CATCH_EXIT_FAIL (_e);
         }
+      Try { gfunc3_real2complex (proj_stack); }  CATCH_EXIT_FAIL (_e);
     }
-  Try { gfunc3_real2complex (proj_stack); }  CATCH_EXIT_FAIL (_e);
+
+
+  /* Initialize the residual stack */
+  Try { gfunc3_init_from_foreign_grid (residual_stack, proj_stack); }  CATCH_EXIT_FAIL (_e);
+
 
 
   /* Compute the integration weights and store them (no endpoint correction) */
@@ -128,7 +125,7 @@ int main(int argc, char **argv)
   theta_cur = angles[1];
   for (i = 0; i < tilts->ntilts; i++)
     {
-      Try { gfunc3_set_stack_pointer (cur_img, proj_stack, i); }  CATCH_EXIT_FAIL (_e);
+      Try { gfunc3_set_stack_pointer (cur_img1, proj_stack, i); }  CATCH_EXIT_FAIL (_e);
 
       /* Get next tilt angles, compute integration weight and scale image accordingly */
       if (i != tilts->ntilts - 1)
@@ -153,77 +150,64 @@ int main(int argc, char **argv)
   printf ("\n\nComputing iteration starting point...\n\n");
   for (i = 0; i < tilts->ntilts; i++)
     {
-      Try { gfunc3_set_stack_pointer (cur_img, proj_stack, i); }  CATCH_EXIT_FAIL (_e);
+      Try { gfunc3_set_stack_pointer (cur_img1, proj_stack, i); }  CATCH_EXIT_FAIL (_e);
       Try { tiltangles_get_angles (tilts, angles, i); }  CATCH_EXIT_FAIL (_e);        
       Try { 
-        et_scattering_adjoint_single_axis (cur_img, angles[1], landw_params->tilt_axis, 
+        et_scattering_adjoint_single_axis (cur_img1, angles[1], landw_params->tilt_axis, 
           et_params, volume, opts->model, weights[i]);
       } CATCH_EXIT_FAIL (_e);
     }
 
 
-  volume_r = gfunc3_realpart (volume, NULL);
-  temp_mrc_out (volume_r, "backproj_r", 0);
-
-  volume_r = gfunc3_imagpart (volume, volume_r);
-  temp_mrc_out (volume_r, "backproj_i", 0);
-
-  /* TODO: continue here */
+  /* Iterate */
   
-  /* Further steps:
-   * - Get initial guess
-   * - back-project
-   * - Start iteration
-   */
+  for (k = 0; k < opts->max_iter; k++)
+    {
+      /* Project current guess */
+      Try { 
+        et_scattering_projection_atonce (volume, tilts, et_params, residual_stack, PROJ_ASSUMPTION);
+      } CATCH_EXIT_FAIL (_e);
+      
+      if (DEBUGGING)
+        temp_mrc_out (residual_stack, "reproj_stk_", k + 1);
+      
+      /* Now image per image */
+      
+      for (i = 0; i < tilts->ntilts; i++)
+        {
+          /* Compute residual */
+          Try { 
+            gfunc3_set_stack_pointer (cur_img1, proj_stack, i);
+            gfunc3_set_stack_pointer (cur_img2, residual_stack, i); 
+          } CATCH_EXIT_FAIL (_e);
+          
+          Try { gfunc3_axpy (-1, cur_img2, cur_img1); }  CATCH_EXIT_FAIL (_e);
+          
+          /* Backproject and add to volume */
+          Try { 
+            et_scattering_adjoint_single_axis (cur_img2, angles[1], landw_params->tilt_axis, 
+              et_params, volume, opts->model, opts->relax_param * weights[i]);
+          } CATCH_EXIT_FAIL (_e);
+        }
+      
+      if (DEBUGGING)
+        {
+          temp_mrc_out (residual_stack, "residual_stk_", k + 1);
+          temp_mrc_out (volume, "volume_", k + 1);
+        }
+      
+      /* TODO: Introduce stopping criterion */
+    }
   
-// 
-// 
-  // /* Scale and shift the volume if necessary; transfer to complex and add imaginary part */
-  // Try {
-    // LandwParams_apply_to_volume (landw_params, volume);
-    // gfunc3_scale_grid (volume, et_params->magnification);
-    // if (volume->type == REAL)
-      // gfunc3_scale (volume, 1.0 + 0.1 * I);  /* TODO: use acr from config file */
-  // }  CATCH_EXIT_FAIL (_e);
-// 
-// 
-  // /* Initialize the stack */
-  // Try {
-    // idx3_copy (proj_stack->shape, landw_params->detector_shape);
-    // proj_stack->shape[2] = tilts->ntilts;
-    // gfunc3_init (proj_stack, NULL, landw_params->detector_px_size, proj_stack->shape, COMPLEX);
-  // } CATCH_EXIT_FAIL (_e);
-// 
-// 
-  // /* Print a summary of everything before starting */
-  // LandwOpts_print (opts);
-  // EtParams_print (et_params);
-  // LandwParams_print (landw_params);
-  // gfunc3_print_grid (volume, "volume grid:");
-  // gfunc3_print_grid (proj_stack, "Projection stack grid:");
-// 
-  // 
-  // /* Compute the projections */
-  // Try {
-    // et_scattering_projection_atonce (volume, tilts, et_params, proj_stack, opts->model);
-  // } CATCH_EXIT_FAIL (_e);
-// 
-  // /* Get the imaginary part, invert contrast if necessary and write it to disk */
-  // Try { proj_stack_imag = gfunc3_imagpart (proj_stack, NULL); }  CATCH_EXIT_FAIL (_e);
-  // if (invert_contrast_flag)
-    // gfunc3_scale (proj_stack_imag, -1.0);
-  // Try { gfunc3_to_mrc (proj_stack_imag, opts->fname_out, NULL); }  CATCH_EXIT_FAIL (_e);
-// 
-  // printf ("\n\nProjections written to %s\n\n", opts->fname_out);
-  // 
-  // /* Delete all objects before exiting */
+
+  /* Delete all objects before exiting */
   LandwOpts_free (&opts);
   EtParams_free (&et_params);
   LandwParams_free (&landw_params);
   tiltangles_free (&tilts);
   gfunc3_free (&volume);
   gfunc3_free (&proj_stack);
-  gfunc3_free (&residual);
+  gfunc3_free (&residual_stack);
   free (weights);
   
   return EXIT_SUCCESS;
